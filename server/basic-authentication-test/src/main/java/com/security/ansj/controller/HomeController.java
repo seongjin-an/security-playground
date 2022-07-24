3154adf3e4fdad8e8d61c91f755397e86454c10f
seongjin-an <seongjin4us@gmail.com>
2022-07-24 16:44:03 +0900

    basic authentication..

---
+++ server/basic-authentication-test/src/main/java/com/security/ansj/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.security.ansj.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("greeting")
+    public String greeting(){
+        return "hello";
+    }
+
+    @PostMapping("greeting")
+    public String me(@RequestBody String name){
+        return "hello " + name;
+    }
+}
